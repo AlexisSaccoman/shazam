ee689d78465b7348196390067fed7d0e7eabe041
WalidMedouaz <145688943+WalidMedouaz@users.noreply.github.com>
2023-12-09 14:32:38 +0100

    Ajout des classes User et Vin

---
+++ lib/Utilisateur.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Utilisateur {
+  String? id;
+  String? nom;
+  String? prenom;
+  String? isConnected;
+  String? isAdmin;
+
+  Utilisateur({
+    this.id,
+    this.nom,
+    this.prenom,
+    this.isConnected,
+    this.isAdmin,
+  });
+
+  Utilisateur.fromJson(Map<String, dynamic> json) {
+    id = json['_id'];
+    nom = json['nom'];
+    prenom = json['prenom'];
+    isConnected = json['isConnected'];
+    isAdmin = json['isAdmin'];
+  }
+
+  static Future<List<Utilisateur>> getUtilisateurs() async {
+    var url = Uri.parse("https://pedago.univ-avignon.fr:3189/findUtilisateurs");
+    final response = await http.get(url, headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Accept': '*/*'
+    });
+    final List body = json.decode(response.body);
+    return body.map((e) => Utilisateur.fromJson(e)).toList();
+  }
+}
---
+++ lib/Vin.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+class Vin extends HttpOverrides {
+  String? id;
+  String? nom;
+  double? tarif;
+  int? millesime;
+  double? volume;
+  String? cepage;
+  List<dynamic>? allergenes;
+  double? teneurEnAlcool;
+  dynamic nomDeDomaine;
+  dynamic provenance;
+  dynamic typeVin;
+  String? ean;
+
+  Vin(
+      {this.id,
+      this.nom,
+      this.tarif,
+      this.millesime,
+      this.volume,
+      this.cepage,
+      this.allergenes,
+      this.teneurEnAlcool,
+      this.nomDeDomaine,
+      this.provenance,
+      this.typeVin,
+      this.ean});
+
+  Vin.fromJson(Map<String, dynamic> json) {
+    id = json['_id'];
+    nom = json['nom'];
+    tarif = json['tarif'];
+    millesime = json['millesime'];
+    volume = json['volume'];
+    cepage = json['cepage'];
+    allergenes = json['allergenes'];
+    teneurEnAlcool = json['teneurEnAlcool'];
+    nomDeDomaine = json['nomDeDomaine'];
+    typeVin = json['typeVin'];
+    ean = json['EAN'];
+  }
+
+  static Future<List<Vin>> getVins() async {
+    var url = Uri.parse("https://pedago.univ-avignon.fr:3189/findVins");
+    final response = await http.get(url, headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Accept': '*/*'
+    });
+    final List body = json.decode(response.body);
+    return body.map((e) => Vin.fromJson(e)).toList();
+  }
+}
